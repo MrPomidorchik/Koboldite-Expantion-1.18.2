41f55f39952bea3d5b09c2dc74bbb896f7902afc
MrPomidorchik <x6x12345@mail.ru>
2022-04-09 16:59:51 +0300

    added more items

--- src/main/java/net/mrpomidorchik/miscellaneous/item/ModItems.java
+++ src/main/java/net/mrpomidorchik/miscellaneous/item/ModItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 import net.mrpomidorchik.miscellaneous.MiscellaneousMod;
 
@@ -11,6 +12,22 @@ public class ModItems {
 
     public static final Item RUBY = registerItem("ruby",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item KOBOLDITE_DUST = registerItem("koboldite_dust",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item KOBOLDITE_NUGGET = registerItem("koboldite_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item KOBOLDITE_INGOT = registerItem("koboldite_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item SKY_FRAGMENT = registerItem("sky_fragment",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC).rarity(Rarity.UNCOMMON)));
+
+
+
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MiscellaneousMod.MOD_ID, name), item);

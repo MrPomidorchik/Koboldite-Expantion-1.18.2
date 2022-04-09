1eb754d60cba114d1eb3e250d7ea28d0be7c09fb
MrPomidorchik <x6x12345@mail.ru>
2022-04-09 18:33:24 +0300

    added ModBlocks class and blocks

--- src/main/java/net/mrpomidorchik/miscellaneous/MiscellaneousMod.java
+++ src/main/java/net/mrpomidorchik/miscellaneous/MiscellaneousMod.java
@@ -1,6 +1,7 @@
 package net.mrpomidorchik.miscellaneous;
 
 import net.fabricmc.api.ModInitializer;
+import net.mrpomidorchik.miscellaneous.block.ModBlocks;
 import net.mrpomidorchik.miscellaneous.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class MiscellaneousMod implements ModInitializer {
 	public void onInitialize() {
 
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
